ff702458593356034b529f1186d32194756144cc
Mshimaz <56425422+Mshimaz@users.noreply.github.com>
2021-05-17 22:55:00 +0530

    Add files via upload

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      drawer: Drawer(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text("Shopping App",style: TextStyle(color: Colors.black),),
+      ),
+    );
+  }
+}
+
